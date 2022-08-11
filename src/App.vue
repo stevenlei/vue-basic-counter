4b7d0512f63b5c356c0709120c204aea8abaa524
Steven Lei <stevenplus@gmail.com>
2022-08-11 22:44:24 +0800

    demo the use of v-model.number

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,57 @@ export default {
     return {
       numberX: 0,
       numberY: 0,
-    }
+    };
   },
   computed: {
     numberZ() {
-      return this.numberX + this.numberY
-    }
-  }
-}
+      return this.numberX + this.numberY;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="min-h-screen w-full bg-slate-100 flex justify-center items-center">
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
-      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="numberX">
+      <input
+        class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none"
+        v-model.number="numberX"
+      />
       <div class="flex">
-        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberX--">-</button>
-        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberX++">+</button>
+        <button
+          class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700"
+          @click="numberX--"
+        >
+          -
+        </button>
+        <button
+          class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700"
+          @click="numberX++"
+        >
+          +
+        </button>
       </div>
     </div>
-    <div class="text-9xl font-mono p-20">
-      +
-    </div>
+    <div class="text-9xl font-mono p-20">+</div>
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
-      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="numberY">
+      <input
+        class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none"
+        v-model.number="numberY"
+      />
       <div class="flex">
-        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberY--">-</button>
-        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberY++">+</button>
+        <button
+          class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700"
+          @click="numberY--"
+        >
+          -
+        </button>
+        <button
+          class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700"
+          @click="numberY++"
+        >
+          +
+        </button>
       </div>
     </div>
     <div class="text-9xl font-mono p-20">
