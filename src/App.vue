cbc25d8a65456f9388c23cde7cae891c388442a2
Steven Lei <stevenplus@gmail.com>
2022-08-11 15:13:18 +0800

    demo the use of .stop modifier

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@ export default {
   <div class="min-h-screen w-full bg-slate-100 flex justify-center items-center">
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
       <h1 class="text-9xl font-semibold text-center text-gray-800 py-12">{{ count }}</h1>
-      <div class="flex">
-        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count--">-</button>
-        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count++">+</button>
+      <div class="flex" @click="count++">
+        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click.stop="count--">-</button>
+        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click.stop="count++">+</button>
       </div>
     </div>
   </div>
