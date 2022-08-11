052d478111663bb48cea4512e8d25b13f51096ee
Steven Lei <stevenplus@gmail.com>
2022-08-11 15:24:49 +0800

    demo the use of v-model

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@ export default {
 <template>
   <div class="min-h-screen w-full bg-slate-100 flex justify-center items-center">
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
-      <h1 class="text-9xl font-semibold text-center text-gray-800 py-12">{{ count }}</h1>
+      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="count">
       <div class="flex">
         <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count--">-</button>
-        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click.exact="count++" @click.shift="count += 10">+</button>
+        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count++">+</button>
       </div>
     </div>
   </div>
