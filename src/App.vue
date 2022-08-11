42c4910446f0c1a9b1d8d40ec539d4648a726cce
Steven Lei <stevenplus@gmail.com>
2022-08-11 16:50:59 +0800

    demo the use of x + y = z

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
 export default {
   data() {
     return {
-      count: 0
+      numberX: 0,
+      numberY: 0,
+    }
+  },
+  computed: {
+    numberZ() {
+      return this.numberX + this.numberY
     }
   }
 }
@@ -11,11 +17,24 @@ export default {
 <template>
   <div class="min-h-screen w-full bg-slate-100 flex justify-center items-center">
     <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
-      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="count">
+      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="numberX">
       <div class="flex">
-        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count--">-</button>
-        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="count++">+</button>
+        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberX--">-</button>
+        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberX++">+</button>
       </div>
+    </div>
+    <div class="text-9xl font-mono p-20">
+      +
+    </div>
+    <div class="bg-white flex-1 max-w-md rounded-xl shadow-xl overflow-hidden">
+      <input class="text-9xl font-semibold text-center text-gray-800 py-12 w-full outline-none" v-model="numberY">
+      <div class="flex">
+        <button class="bg-blue-600 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberY--">-</button>
+        <button class="bg-blue-500 py-4 font-mono text-center flex-1 text-5xl text-white hover:bg-blue-700" @click="numberY++">+</button>
+      </div>
+    </div>
+    <div class="text-9xl font-mono p-20">
+      = <span class="font-sans font-bold">{{ numberZ }}</span>
     </div>
   </div>
 </template>
